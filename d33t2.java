81bc3ae68d4d1a2e5ab46ed9c3ec75ad79b08bd9
samirhembrom <samirhembrom007@gmail.com>
2023-02-04 14:44:31 +0530

    Day 33

---
+++ d33t2.java
@@ -0,0 +1,31 @@
+//81. Search in Rotated Sorted Array II
+class Solution {
+    public boolean search(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        while ( low <= high )
+        {
+            int mid = low + (high - low) / 2;
+            if ( arr[mid] == target ) return true;
+            if ( arr[low] == arr[mid] && arr[high] == arr[mid] ) { low++; high--; }
+            else if ( arr[low] <= arr[mid] )
+            {   
+                if ( arr[low] <= target && arr[mid] > target )
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }    
+            else 
+            {
+                if ( arr[mid] < target && arr[high] >= target )
+                    low = mid + 1;
+                else 
+                    high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
+/*
+Time Complexity:  O(log n) Using binary search
+Space Complexity: O(1) No extra space is required
+*/
